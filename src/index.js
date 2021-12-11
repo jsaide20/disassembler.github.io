f7dc8ed3828061c9e1a4c7dd6000f5e67cca0d0d
jsaide20 <64879770+jsaide20@users.noreply.github.com>
2021-12-11 01:00:33 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-	<HashRouter base ="/">
     <App />
-	</HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
